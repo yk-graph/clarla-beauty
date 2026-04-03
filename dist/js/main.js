5dce242c7a2cc5ab5e3866f745eec8b8c3f5438a
yk-graph <yokokura.tatsuya@gmail.com>
2026-04-03 12:02:44 -0700

    add: implement header scroll animation and functionality

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,11 @@
 import { contactFormHandler } from './modules/contact-form.js'
 import { toggleHeaderMenu } from './modules/toggle-header-menu.js'
+import { initHeader } from './modules/header.js'
 
 function init() {
   toggleHeaderMenu()
   contactFormHandler()
+  initHeader()
 }
 
 init()
---
+++ dist/js/modules/header.js
@@ -0,0 +1,32 @@
+/**
+ * ヘッダーのスクロール制御
+ */
+export function initHeader() {
+  const header = document.getElementById('header')
+  if (!header) return
+
+  const SCROLL_THRESHOLD = 50 // 50px
+
+  function handleScroll() {
+    const currentScrollY = window.scrollY
+
+    if (currentScrollY >= SCROLL_THRESHOLD) {
+      header.classList.add('is-scrolled')
+    } else {
+      header.classList.remove('is-scrolled')
+    }
+  }
+
+  let ticking = false
+  window.addEventListener('scroll', () => {
+    if (!ticking) {
+      window.requestAnimationFrame(() => {
+        handleScroll()
+        ticking = false
+      })
+      ticking = true
+    }
+  })
+
+  handleScroll()
+}
